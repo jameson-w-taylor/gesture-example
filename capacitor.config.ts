9b48549340dc4bb24259c7e44ea2899665c1af77
Jameson Taylor <jameson@ionic.io>
2024-08-20 09:59:29 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'gesture-example',
+  webDir: 'dist'
+};
+
+export default config;
